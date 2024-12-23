f95a8a32fe5fa43d0904f8167e6de24b8ddd3df4
Johan901 <johan_ede.hurtado@uao.edu.co>
2024-12-23 12:42:39 -0500

    Added Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dg/separados',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '299715710ffa7fc0d6bbbabf1c84abf72bd199bd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dg/separados' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '299715710ffa7fc0d6bbbabf1c84abf72bd199bd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

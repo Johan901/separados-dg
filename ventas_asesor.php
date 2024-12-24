6a5dd46fd65ba2e6e27f57f53e3c34e135fd13b4
Johan901 <johan_ede.hurtado@uao.edu.co>
2024-12-24 10:47:00 -0500

    Update Reports

--- ventas_asesor.php
+++ ventas_asesor.php
@@ -4,16 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel de Reportes - DG</title>
-    <link rel="stylesheet" href="css/styles_reportes.css?v=5.0">
+    <link rel="stylesheet" href="css/styles_reportes.css?v=2.0">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://www.gstatic.com/charts/loader.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
-    <script src="js/main_user.js?v=4.0"></script>
+    <script src="js/main_user.js?v=2.1"></script>
 
 </head>
 <body>
@@ -77,7 +72,6 @@
 
     <script>
     $(document).ready(function () {
-        
         // Reporte por Asesor
         $('#filtro-asesor').on('submit', function (e) {
             e.preventDefault();

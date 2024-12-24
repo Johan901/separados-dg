f20922018473864f481a6c2188821a302599c138
Johan901 <johan_ede.hurtado@uao.edu.co>
2024-12-24 10:00:56 -0500

    Update Reports

--- separados_conjunto.php
+++ separados_conjunto.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 // Preparar datos para el gráfico
                 $chartDataConjunto = [["Asesor", "Número de Pedidos", ["role" => "style"]]];
-                $colors = ['#00796b', '#a31010', '#00332b', '#a31010', '#e91d29'];
+                $colors = ['#e91d29', '#a31010', '#00332b', '#a31010', '#e91d29'];
                 $colorIndex = 0;
 
                 foreach ($ventasConjunto as $venta) {

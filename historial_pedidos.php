d08f596c2983ac38350f097900405bb1d4437c5c
Johan901 <johan_ede.hurtado@uao.edu.co>
2025-01-08 15:20:33 -0500

    Update Reports

--- historial_pedidos.php
+++ historial_pedidos.php
@@ -82,22 +82,12 @@ $cliente_cedula = null;
 $message = '';
 
 // Consulta básica
-$query = "SELECT p.id_pedido, 
-       p.cliente_cedula, 
-       p.fecha_pedido, 
-       p.fecha_limite,
-       SUM(dp.subtotal) AS total_pedido, 
-       p.asesor, 
-       p.envio,
-       c.nombre AS nombre_cliente, 
-       p.estado,
-       p.observacion  -- Usando directamente la columna 'observacion' de la tabla 'pedidos'
-FROM pedidos p 
-LEFT JOIN detalle_pedido dp ON p.id_pedido = dp.id_pedido 
-LEFT JOIN clientes c ON p.cliente_cedula = c.cedula
-GROUP BY p.id_pedido, p.cliente_cedula, p.fecha_pedido, p.fecha_limite, 
-         p.asesor, p.envio, c.nombre, p.estado, p.observacion;
-
+$query = "SELECT p.id_pedido, p.cliente_cedula, p.fecha_pedido, p.fecha_limite,
+                 SUM(dp.subtotal) AS total_pedido, p.asesor, p.envio,
+                 c.nombre AS nombre_cliente, p.estado, p.observaciones
+          FROM pedidos p 
+          LEFT JOIN detalle_pedido dp ON p.id_pedido = dp.id_pedido 
+          LEFT JOIN clientes c ON p.cliente_cedula = c.cedula";
 
 $conditions = [];
 

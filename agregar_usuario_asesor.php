84edccba99cb00fe92843be14341ef2e71cbd92b
Johan901 <johan_ede.hurtado@uao.edu.co>
2025-02-25 10:23:55 -0500

    Update Reports

--- agregar_usuario_asesor.php
+++ agregar_usuario_asesor.php
@@ -144,9 +144,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Manejar la respuesta después de que se haya enviado el formulario
     window.onload = function() {
         <?php if ($response == "success") : ?>
-            swal("Éxito!", "Cliente agregado con éxito.", "success").then(() => {
-                window.location.href = 'admin_panel.php';
+            swal({
+                title: "Éxito!",
+                text: "Cliente agregado con éxito.",
+                type: "success"
+            }, function() {
+                window.location.href = 'nuevo_pedido.php';
             });
+
         <?php elseif ($response == "duplicate") : ?>
             swal("Error!", "El cliente con esta cédula ya está registrado.", "error");
         <?php elseif (strpos($response, "error") !== false) : ?>

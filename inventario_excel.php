6cb00665ae523ff4a68d92bad6de035234e56bff
Johan901 <johan_ede.hurtado@uao.edu.co>
2025-03-29 09:54:18 -0500

    Update Reports

--- inventario_excel.php
+++ inventario_excel.php
@@ -4,13 +4,21 @@ include('config.php');
 header("Content-Type: application/vnd.ms-excel");
 header("Content-Disposition: attachment; filename=Inventario_Dispo.xls");
 
-$query = "SELECT * FROM inventario WHERE cantidad > 0 ORDER BY cantidad DESC";
+// Nueva consulta con las columnas correctas
+$query = "SELECT ref, tipo_prenda, color, cantidad, precio_al_detal, precio_por_mayor, fecha_creacion 
+          FROM inventario 
+          WHERE cantidad > 0 
+          ORDER BY cantidad DESC";
+
 $stmt = $conn->prepare($query);
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo "ID\tProducto\tCantidad\tPrecio\n";
+// Imprimir encabezados de columnas
+echo "Referencia\tTipo de Prenda\tColor\tCantidad\tPrecio al Detal\tPrecio por Mayor\tFecha de Creación\n";
+
+// Imprimir datos
 foreach ($result as $row) {
-    echo "{$row['id']}\t{$row['producto']}\t{$row['cantidad']}\t{$row['precio']}\n";
+    echo "{$row['ref']}\t{$row['tipo_prenda']}\t{$row['color']}\t{$row['cantidad']}\t{$row['precio_al_detal']}\t{$row['precio_por_mayor']}\t{$row['fecha_creacion']}\n";
 }
 ?>
